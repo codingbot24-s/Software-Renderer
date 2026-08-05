3982ac12fd76d37870b78dbe47a1f732d6349b04
saadwsl <primecoder8@gmail.com>
2026-08-05 13:19:50 +0000

    Add todos for filling triangle

--- main.c
+++ main.c
@@ -2,10 +2,18 @@
 // TODO: we need to organize our code
 
    /*
-      TODO: Implement a function that draws the outline of a
-      triangle by drawing its three edges using your line-drawing function.
+      Implement a function that fills a solid triangle so every pixel inside the triangle is colored,
+      regardless of the triangle's orientation or vertex order.
    */
 
+// we will be doing this with scanline algo
+// 1. bring a on the top by sorting
+// 2. bring b on the middle 
+// 3. first check if the triangle is not flat
+//    then because we have y get the two x of the
+//    triangle by interpolation
+// 4. then check if the triangle is not the flat bottom
+//    do the same thing there 
 #include "sdl_init.h"
 
 int main () {
