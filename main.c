f2d92a95195912a329683290d8a30156799206a9
saad <primecoder8@gmail.com>
2026-08-08 13:30:33 +0530

    add todo

--- main.c
+++ main.c
@@ -1,21 +1,10 @@
 
-// TODO: we need to organize our code
-
-  /*
-      Implement a depth (Z) buffer, clear it every frame, and use it so that overlapping triangles are 
-      rendered correctly based on depth instead of draw order.
-  */
-
 
 /*
-  we need to interpolate z also for completing this task
-  1. sort z also in our sort function
-  2. get the depth of left right edge by same interpolation
-  3. and each pixel between them by same interpolation
-  4. check the z in the z buffer and then draw pixel 
+  Define a 3D vertex representation containing position (x, y, z) 
+  and implement a transformation system that can translate, rotate, and scale vertices in 3D space.
 */
 
-// i think the task is complete 
 #include "sdl_init.h"
 
 int main () {
